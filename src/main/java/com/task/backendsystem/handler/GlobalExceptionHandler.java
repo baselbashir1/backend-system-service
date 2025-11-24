8dd904b2c6b0cd31dc058205b3d324fb75fefdb8
Basel Bashir <eng.baselbashir@gmail.com>
2025-11-24 23:51:21 +0300

    Added GlobalExceptionHandler

---
+++ src/main/java/com/task/backendsystem/handler/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.task.backendsystem.handler;
+
+import com.task.backendsystem.dto.response.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage(), 404, null), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralException(Exception e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage(), 424, null), HttpStatus.FAILED_DEPENDENCY);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .toList();
+        return new ResponseEntity<>(new ErrorResponse("Validation errors", 400, errors), HttpStatus.BAD_REQUEST);
+    }
+}
